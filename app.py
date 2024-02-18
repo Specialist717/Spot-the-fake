3019fe95cea5d5437dab81e5f2a2b2f87e2a5a44
Specialist717 <pawelgor987@gmail.com>
2024-02-18 21:36:50 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import random
+from statements import true_statements, false_statements
+
+app = Flask(__name__) 
+
+score = 0
+
+def picking_statements(true_statements, false_statements):
+    true_s = random.choice(true_statements)
+    false_s = random.choice(false_statements)
+    true_statements.remove(true_s)
+    false_statements.remove(false_s)
+    return [true_s, false_s]
+
+def statment_order():
+    order_top = random.randint(0, 1)
+    if order_top == 0:
+        order_bottom = 1
+    else:
+        order_bottom = 0
+    return order_top, order_bottom  
+
+@app.route('/')
+def index():
+    return render_template('start_page.html')
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    global score
+    if request.method == 'GET':
+        statements = picking_statements(true_statements, false_statements)
+        order_top, order_bottom = statment_order()
+        return render_template('game_page.html', score=score, statements = statements, order_top = order_top, order_bottom = order_bottom)
+    elif request.method == 'POST':
+        if 'top' in request.form and bool(statements[order_top][4]) == True or 'bottom' in request.form and bool(statements[order_bottom][4]) == True:
+            score += 1
+        else:
+            score = 0
+        order_top, order_bottom = statment_order()
+        statements = picking_statements(true_statements, false_statements)
+    return render_template('game_page.html', score=score, statements = statements, order_top = order_top, order_bottom = order_bottom)
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    return render_template('stats_page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ statements.py
@@ -0,0 +1,25 @@
+# [id, type, statement, source, true/false]
+true_statements = [
+    [1, "myth", "The Earth is approximately 4.5 billion years old.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [2, "myth", "Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [3, "myth", "A group of flamingos is called a 'flamboyance.'", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [4, "myth", "The shortest war in history was between Britain and Zanzibar on August 27, 1896, lasting only 38 minutes.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [5, "myth", "Octopuses have three hearts.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [6, "myth", "The Eiffel Tower can be 15 cm taller during the summer due to thermal expansion of the iron.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [7, "myth", "The honeybee is the only insect that produces food eaten by humans.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [8, "myth", "Cows have best friends and can become stressed when they are separated.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [9, "myth", "The Great Wall of China is not visible from the moon with the naked eye.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True],
+    [10, "myth", "A day on Venus is longer than a year on Venus.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", True]
+]
+false_statements = [
+    [1, "myth", "The Great Wall of China is visible from the moon.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [2, "myth", "Bananas grow on trees.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [3, "myth", "Goldfish have a three-second memory span.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [4, "myth", "Einstein failed mathematics in school.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [5, "myth", "Bulls are enraged by the color red.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [6, "myth", "Humans only use 10% of their brains.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [7, "myth", "Lightning never strikes the same place twice.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [8, "myth", "Chameleons change color to blend in with their surroundings.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [9, "myth", "Vikings wore helmets with horns.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False],
+    [10, "myth", "You can catch a cold by being in the cold.", "https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna", False]
+] 
